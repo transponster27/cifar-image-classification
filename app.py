502948ce767b8247fb3bf204cc0a8bf15f1c0c4f
transponster27 <transponster27@gmail.com>
2026-06-15 13:37:09 +0100

    Add application file

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
 import gradio as gr
 
-from src.predict import (
-    load_trained_model,
-    predict_single_image
-)
+def greet(name):
+    return "Hello " + name + "!!"
 
-model = load_trained_model()
-
-def predict(image):
-
-    prediction, confidence, _ = (
-        predict_single_image(
-            model,
-            image
-        )
-    )
-
-    return f"{prediction} ({confidence:.4f})"
-
-demo = gr.Interface(
-    fn=predict,
-    inputs=gr.Image(type="numpy"),
-    outputs="text",
-    title="CIFAR10 Classifier"
-)
-
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
